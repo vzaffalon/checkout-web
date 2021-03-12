4fda9e99c1b32738d86e77ef1b179378fc8a3eca
Victor Zaffalon <victor.zaffalon@udlab.com.br>
2021-03-11 21:50:42 -0300

    Load categories and items from apapi

--- src/App.js
+++ src/App.js
@@ -3,14 +3,34 @@ import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import theme from "./theme.js"
+import bucketUri from "./utils/s3-bucket.js"
+import { Item, Category, Order } from './models/index.js';
 
 function App() {
   const [selectedTab, setSelectedTab] = useState(0)
   const [items, setItems] = useState([])
+  const [categories, setCategories] = useState([])
 
   const handleChange = (event, newValue) => {
     setSelectedTab(newValue)
+    getItems(categories[newValue].id);
   }
+
+  const getItems = (category_id) => {
+    Item.list({ category_id: category_id }).then(response => {
+      setItems(response.data)
+    })
+  }
+
+  const getCategories = () => {
+    Category.list().then(response => {
+      setCategories(response.data)
+    })
+  }
+
+  useEffect(() => {
+    getCategories();
+  }, []);
 
   const useStyles = makeStyles((theme) => ({
     root: {
@@ -32,25 +52,24 @@ function App() {
   const ItemsList = () => {
     const classes = useStyles();
 
-    return(<GridList cellHeight={180} className={classes.gridList}>
-        <GridListTile key="Subheader" cols={3} style={{ height: 'auto' }}>
-          <ListSubheader component="div">December</ListSubheader>
+    return (<GridList cellHeight={180} className={classes.gridList}>
+      <GridListTile key="Subheader" cols={3} style={{ height: 'auto' }}>
+        <ListSubheader component="div">December</ListSubheader>
+      </GridListTile>
+      {items.map((item) => (
+        <GridListTile key={item.id}>
+          <img src={bucketUri + item.image_id + ".jpg"} alt={"title"} />
+          <GridListTileBar
+            title={item.name}
+            subtitle={<span>$ {item.price}</span>}
+            actionIcon={
+              <div></div>
+            }
+          />
         </GridListTile>
-        {items.map((tile) => (
-          <GridListTile key={tile.img}>
-            <img src={tile.img} alt={"title"} />
-            <GridListTileBar
-              title={"title"}
-              subtitle={<span>$ 50,00</span>}
-              actionIcon={
-                <div></div>
-              }
-            />
-          </GridListTile>
-        ))}
-      </GridList>)
+      ))}
+    </GridList>)
   }
-
 
   return (
     <MuiThemeProvider theme={theme}>
@@ -63,18 +82,10 @@ function App() {
           variant="fullWidth"
           aria-label="disabled tabs example"
         >
-          <Tab label="All" >
+          {categories.map((category) => <Tab label={category.name} >
             <ItemsList></ItemsList>
-          </Tab>
-          <Tab label="Bakery">
-            <ItemsList></ItemsList>
-          </Tab>
-          <Tab label="Entrees">
-            <ItemsList></ItemsList>
-          </Tab>
-          <Tab label="Drinks" >
-            <ItemsList></ItemsList>
-          </Tab>
+          </Tab>)
+          }
         </Tabs>
       </div>
     </MuiThemeProvider>
