1a01572275b596c28ba9cbfecf2128c70731c341
Victor Zaffalon <victor.zaffalon@udlab.com.br>
2021-03-14 11:28:23 -0300

    Remove unused axios model method

--- src/models/Order.js
+++ src/models/Order.js
@@ -3,12 +3,6 @@ import axios from 'axios';
 
 const model_uri = "orders/"
 
-const list = async (params) => {
-    return new Promise(async (resolve, reject) => {
-        resolve(axios.get(`${api.uri}${model_uri}`,{params: params}));
-    });
-}
-
 const create = async (payload) => {
     return new Promise(async (resolve, reject) => {
         resolve(axios.post(`${api.uri}${model_uri}`,payload));
